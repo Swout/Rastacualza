1b6de63d0d115cbe1baed6dbdbe4e5aa611ebf24
Oscar Portillo <66044741+Swout@users.noreply.github.com>
2020-08-04 09:57:59 -0600

    Inialización de eventos para firebase

--- app/src/main/java/com/swout/rastacualza/MainActivity.kt
+++ app/src/main/java/com/swout/rastacualza/MainActivity.kt
@@ -2,10 +2,15 @@ package com.swout.rastacualza
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("messege","Integracion Completa")
+        analytics.logEvent("InitScreen",bundle)
     }
 }
