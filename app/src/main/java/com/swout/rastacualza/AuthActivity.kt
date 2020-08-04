f50b60fd47d08337135c0f6b3e53b89614b32a71
Oscar Portillo <66044741+Swout@users.noreply.github.com>
2020-08-04 10:40:28 -0600

    Implementacion de la Autentificacion por correo

--- app/src/main/java/com/swout/rastacualza/AuthActivity.kt
+++ app/src/main/java/com/swout/rastacualza/AuthActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
         val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
         val bundle = Bundle()
         bundle.putString("messege","Integracion Completa")
